832a5f0c352ebc505b7067224af61531dfcf5897
Madeleine Woodbury <woodbu7@ad.nmc.edu>
2018-04-02 16:14:01 -0400

    Add project files.

---
+++ TheAionProject.S3Plus_Starter/Models/TravelerObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject
+{
+    public class TravelerObject : GameObject
+    {
+        public override int Id { get; set; }
+        public override string Name { get; set; }
+        public override string Description { get; set; }
+        public override int SpaceTimeLocationId { get; set; }
+        public TravelerObjectType Type { get; set; }
+        public string PickUpMessage { get; set; }
+        public string PutDownMessage { get; set; }
+        public bool CanInventory { get; set; }
+        public bool IsConsumable { get; set; }
+        public bool IsVisible { get; set; }
+        public int Value { get; set; }
+
+        public event EventHandler ObjectAddedToInventory;
+
+        public void OnObjectAddedToInventory()
+        {
+            if (ObjectAddedToInventory != null)
+            {
+                ObjectAddedToInventory(this, EventArgs.Empty);
+            }
+        }
+    }
+}
